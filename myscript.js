e008f22eef6e37171ac9440174be7fbf08fd7246
BubovaLeon <60527436+BubovaLeon@users.noreply.github.com>
2020-09-14 12:44:40 +0200

    initial commit

---
+++ myscript.js
@@ -0,0 +1,36 @@
+$(document).scroll(function() {
+	var y = $(this).scrollTop();
+	if (y > 550 && $( window ).width() < 1160){
+		$('#extra-header').fadeIn();
+	} else {
+		$('#extra-header').fadeOut();
+	}
+
+	if (y > 1 && $( window ).width() >= 1160) {
+		$('#navigation').addClass('moved-navigation')
+	} else {
+		$('#navigation').removeClass('moved-navigation')
+	}
+	
+      });
+
+$(window).resize(function(){
+	checkHeaders();
+});
+
+$(document).ready(function(){
+	checkHeaders();
+});
+
+function checkHeaders(){
+	var y = $(this).scrollTop();
+	if ( $(window).width() >= 1160){
+		$('#navigation').css({position:'fixed'});
+		$('#extra-header').fadeOut();
+	} else {
+		$('#navigation').css({position:'absolute'});
+		if (y > 550){
+			$('#extra-header').fadeIn();
+		}
+	}
+}
